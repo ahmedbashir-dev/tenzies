e19b3c10bd408321d5f7daee975258627c12ae61
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-25 20:32:49 +0500

    Rendered dice elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
+import { useState } from "react";
 import { Die } from "./component/Die"
 export default function App() {
-  function generateAllNewDice(){
+  
+  const [diceValues, setDiceValues] = useState(generateRandomNumbersArray());
+
+
+  function generateRandomNumbersArray(){
     const randomNumbers = Array.from({length:10}, () => Math.floor(Math.random() * 6) + 1);
-    console.log(randomNumbers);
+    return randomNumbers
   }
 
-  generateAllNewDice();
+  const diceElements = diceValues.map((value, index) => {
+    return <Die key={index} value={value} />
+  })
+
+  
   return (
     <main>
         <h2>Tenzies</h2>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElements}
       </div>
     </main>
   )
