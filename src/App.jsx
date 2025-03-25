bb59f23aba185bef0ce15b18455f1c17851b5832
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-26 02:34:16 +0500

    Accessbility improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 import { Die } from "./component/Die"
@@ -6,7 +6,7 @@ import { Die } from "./component/Die"
 export default function App() {
   
   const [dice, setDice] = useState(() => generateRandomNumbersArray());
-
+  const buttonRef = useRef(null);
 
   function hold(id){
     setDice(prevDice => (prevDice.map(die => (
@@ -41,16 +41,25 @@ export default function App() {
 
   const gameWon = dice.every(die => die.value === dice[0].value) && dice.every(die => die.isHeld);
 
+  useEffect(() => {
+    if(gameWon){
+      buttonRef.current
+      .focus();
+    }
+  }, [gameWon]);
   
   return (
     <main>
       {gameWon && <Confetti />}
+      <div aria-live="polite">
+        {gameWon && <p role="alert" className="sr-only">You won! Press New Game to start new game</p>} 
+      </div>
         <h2>Tenzies</h2>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button onClick={rollDice} className="roll-die-button">{gameWon ? "New Game": "Roll"}</button>
+      <button ref={buttonRef} onClick={rollDice} className="roll-die-button">{gameWon ? "New Game": "Roll"}</button>
     </main>
   )
 
--- src/component/Die.jsx
+++ src/component/Die.jsx
@@ -1,5 +1,9 @@
 export function Die(props){
-    return(<button onClick={()=>props.hold(props.id)} className={`die ${props?.isHeld ? "is-held" : ""}`}>
+    return(<button  
+        aria-label={`Die with value ${props.value}, 
+        ${props.isHeld ? "held" : "not held"}`} 
+        aria-pressed={props.isHeld}
+        onClick={()=>props.hold(props.id)} className={`die ${props?.isHeld ? "is-held" : ""}`}>
          <span>{props?.value}</span>
         </button>
     )
